bbacdf82a061622f49030dc7c78eb8941950a76c
Rupali Umagol <rupali.umagol@gmail.com>
2024-05-09 21:39:55 +0530

    Resolve conflict

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("In Class Tes1");
 		System.out.println("Line 6 change in Test1");
+		System.out.println("Branch B line 7 change");
 	}
 }
