d3c00d6e1dc93fadd16ae56d966622f10c832dec
Rupali Umagol <rupali.umagol@gmail.com>
2024-05-10 15:50:35 +0530

    branch 2 chnages

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -3,5 +3,7 @@ package p2;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("In Class Tes2");
+		System.out.println("In barncha B line change 6");
+		System.out.println("In Barnch B line 7 change ");
 	}
 }
